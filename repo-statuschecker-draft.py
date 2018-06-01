116c6899407b3b0d8006c9e2f9ab0fc481095d1a
Vipul Gupta (@vipulgupta2048) <vipulgupta2048@gmail.com>
2018-06-01 14:56:02 +0530

    Add Documentation - draft | License to script | bug fixes | requirements.txt
    Signed-off-by: Vipul Gupta (@vipulgupta2048) <vipulgupta2048@gmail.com>

--- repo-statuschecker-draft.py
+++ repo-statuschecker-draft.py
@@ -1,25 +1,38 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018, Vipul Gupta
+
+# This file is part of SugarPort.
+
+# SugarPort is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# SugarPort is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with SugarPort.  If not, see <http://www.gnu.org/licenses/>.
+
 from github import Github
 from github.GithubException import GithubException
 import configparser
 
-# Author: vipulgupta2048
-# Date: 30/05/18
-# Libraries needed - ConfigParser, PyGitHub
-# run "pip3 install pygithub configparser --user"
-
-'''Data such as missing license mentions, abstract of activities from
- the wikis, missing links from help activity, missing sections etc.
- In addition to this, ensuring that activity.info file is consistent and
- containing all information needed'''
+'''Data such as missing license mentions, missing links from
+ help activity, missing sections etc. In addition to this, ensuring that
+  activity.info file is consistent and containing all information needed'''
 
 cp = configparser.ConfigParser()
-activityFile = 'activity/activity.info'
 
+activityFile = 'activity/activity.info'
 SECTIONS = 'Activity'
 OPTIONS = [
             'name', 'activity_version', 'bundle_id', 'icon', 'exec',
             'mime_types', 'license', 'summary', 'update_url', 'tags',
-            'max_participants', 'repository ', 'tags', 'single_instance']
+            'max_participants', 'repository', 'tags', 'show_launcher',
+            'categories', 'category' ,'screenshots','single_instance', 'website']
 
 
 def analyse_activityinfo(repo):
@@ -45,7 +58,7 @@ def analyse_activityinfo(repo):
 
 def findLicense(repo):
     try:
-        repo.get_file_contents('LICENSE')
+        repo.get_file_contents('LICENSE') or repo.get_file_contents('COPYING')
         print("License found in the repo")
     except GithubException as error:
         print("License not found in the repo")
@@ -53,8 +66,8 @@ def findLicense(repo):
 
 def screenshot(repo):
     try:
-        repo.get_file_contents('/screenshot')
-        print("Screenshot directory found in the repo")
+        repo.get_dir_contents('screenshot/') or repo.get_dir_contents('screenshots/')
+        print("Screenshots directory found in the repo")
     except GithubException as error:
         print("Screenshot directory not found in the repo")
 
@@ -71,29 +84,31 @@ def readme(repo):
 
 # def summary(repo):
     # tried various methods to extract summary of an activity but
-    # couldn't (Methods used configparser, scraping, raw_extract)
+    # couldn't (Methods used configparser, scraping, raw extract)
 
 def activity(repo):
     '''Return True if the repository is an activity, else false'''
     try:
         repo.get_file_contents(activityFile)
         print("Activity found - %s" % repo.html_url)
-        findLicense(repo)
-        readme(repo)
-        screenshot(repo)
-        analyse_activityinfo(repo)
     except Exception as e:
         print("Not an activity - %s" % repo.html_url)
+        return
+    findLicense(repo)
+    readme(repo)
+    screenshot(repo)
+    analyse_activityinfo(repo)
 
 
 if __name__ == '__main__':
     # Enter your access token here
-    g = Github("3c3b561923fc2536301ffa37a16d92f3cbadee7f")
+    g = Github("1a3b9b5227e60c6b4e5375e944e95e18732cd677")
     # Replace with organisation name
     sugar_activities = g.get_organization("sugarlabs")
-    # file = open('Results.txt', 'w')
+    # Searching all repo of the organiztion
+    counter = 0
     for repo in sugar_activities.get_repos():
+        counter = counter + 1
         print("---------------------------------------------------------")
+        print("No of Repositories searched - {}".format(counter))
         activity(repo)
-        # Repository names not containing README.md recorded
-        # file.write("\n " + repo.html_url)
